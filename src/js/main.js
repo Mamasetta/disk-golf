9aa6f15c5cd2a0260b12543a2c3a6fb02083f215
Varvara Bazhukova <mamasetta@gmail.com>
2017-03-21 22:04:28 +0500

    create

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+//= partials/jquery.min.js
+//= partials/jquery-migrate.min.js
+//= partials/jquery.scrollTo.min.js
+//= partials/bootstrap.min.js
+//= partials/device.js
+//= partials/masonry.pkgd.min.js
+//= partials/page.js
